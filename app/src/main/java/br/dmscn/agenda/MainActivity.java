968dcbc07ab62dd7253229ba483ec0589262d29b
dmscn-faeterj <virtual.id.dsc@gmail.com>
2017-06-18 20:30:05 -0300

    upgraded mainactivity to multilayout

--- app/src/main/java/br/dmscn/agenda/MainActivity.java
+++ app/src/main/java/br/dmscn/agenda/MainActivity.java
@@ -5,22 +5,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     Button btnLogin;
     Button btnSignUp;
+    LinearLayout layMainButtons;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        layMainButtons = (LinearLayout) findViewById(R.id.layMainButtons);
         btnLogin = (Button) findViewById(R.id.btnCallLogin);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Login.class);
-                startActivity(intent);
+                LinearLayout layLogo;
+                LinearLayout layInputs;
+                LinearLayout layButtons;
+
+                layLogo = (LinearLayout) findViewById(R.id.layLogo);
+                layInputs = (LinearLayout) findViewById(R.id.layLoginInputs);
+                layButtons = (LinearLayout) findViewById(R.id.layLoginButtons);
+
+                layMainButtons.setVisibility(View.GONE);
+                layLogo.setWeightSum(1);
+                layInputs.setVisibility(View.VISIBLE);
+                layButtons.setVisibility(View.VISIBLE);
             }
         });
 
@@ -29,8 +43,18 @@ public class MainActivity extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignUp.class);
-                startActivity(intent);
+                LinearLayout layLogo;
+                LinearLayout layInputs;
+                LinearLayout layButtons;
+
+                layLogo = (LinearLayout) findViewById(R.id.layLogo);
+                layInputs = (LinearLayout) findViewById(R.id.laySignUpInputs);
+                layButtons = (LinearLayout) findViewById(R.id.laySignUpButtons);
+
+                layMainButtons.setVisibility(View.GONE);
+                layLogo.setWeightSum(1);
+                layInputs.setVisibility(View.VISIBLE);
+                layButtons.setVisibility(View.VISIBLE);
             }
         });
     }
